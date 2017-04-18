7a2aca291f5150f665419903bc6c8e6c869e5ebf
Bisma <bismasaeed00@gmail.com>
2017-04-18 15:53:50 +0500

    Multiple Selections on Calendar
    implement delegates for selection of multiple selections of dates.
    Selection can be restricted for some specific dates.

--- Islamic Calendar Example/Islamic Calendar Example/ViewController.h
+++ Islamic Calendar Example/Islamic Calendar Example/ViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import "BSIslamicCalendar.h"
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<BSIslamicCalendarDelegate>
 
 
 @end
